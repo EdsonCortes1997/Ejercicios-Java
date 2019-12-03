ed01e008455eccc960c989e62b71a8bc68aa79fa
EdsonCortes1997 <57964346+EdsonCortes1997@users.noreply.github.com>
2019-12-03 16:53:48 -0600

    Add files via upload

---
+++ Ejer1.java
@@ -0,0 +1,13 @@
+public class Ejer1{
+
+	public static void main (String [] args ){
+		int num = 10;
+	float flota = 12.5f;
+	String javita = "Java programming";
+	
+	System.out.println(num);
+	System.out.println(flota);
+	System.out.println(javita);
+	
+	}
+}
---
+++ Ejer2.java
@@ -0,0 +1,11 @@
+public class Ejer2{
+
+	public static void main (String [] args ){
+	String hi = "Hi";
+	String bye ="Bye";
+	
+	System.out.print(hi+bye+bye+hi);
+	
+	
+	}
+}
